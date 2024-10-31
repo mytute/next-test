945df4cb012fa03d26a3e94ad4ccf0150a0562a0
samadhi laksahan <samadhivkcom@gmail.com>
2024-10-31 22:53:59 +0530

    add test:

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -4,10 +4,6 @@ export default function Home () {
   return (
     <>
       <h1>Home page</h1>
-     <Link href="/blog">Blog</Link>
-     <h1><Link href="/products/1">Product detail 1</Link></h1>
-     <h1><Link href="/products/2">Product detail 2</Link></h1>
-     <h1><Link href="/products/3" replace>Product detail 3</Link></h1>
     </>
   )
 }
